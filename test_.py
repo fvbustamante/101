241e00f68452ff73102f5c22f7a0ff1b66af188f
Felipe Bustamante <fbustamante@FelipesMac.local>
2019-01-25 03:44:55 -0300

    Introducción de algunos archivos de prueba

---
+++ test_.py
@@ -0,0 +1,18 @@
+#%%
+dic={}
+
+
+lista=["Felipe", "Bustamante", "34"]
+
+for elemento in lista:
+    dic={
+    'Nombre':lista[0],
+    'Apellido':lista[1],
+    'Edad':lista[2],  
+    }
+
+
+
+
+
+dic["Edad][[´"]
